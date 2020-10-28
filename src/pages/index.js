c157c0a136afed93b3409ea90d14a99262737437
Jonathan Luk <jluk87@gmail.com>
2020-10-28 00:36:59 -0400

    First commit.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import styled, { createGlobalStyle } from "styled-components"
 
 const IndexPage = () => (
   <Layout>
